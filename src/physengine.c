1b749bc5eb85b16bf667adee3de662f2ec783df6
atomnuker <atomnuker@gmail.com>
2013-12-09 17:14:54 +0000

    Switched to dynamic arrays, rewrote integrator, support for n-dimensional systems

--- src/physengine.c
+++ src/physengine.c
@@ -9,79 +9,78 @@
 #include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_ttf.h>
 
-#define obj 4
 
-double posx[obj], velx[obj], accx[obj], forcex[obj];
-double posy[obj], vely[obj], accy[obj], forcey[obj];
-double posz[obj], velz[obj], accz[obj], forcez[obj];
-double accprevx[obj], forcecouloumbx[obj], forcegravx[obj], diffx[obj];
-double accprevy[obj], forcecouloumby[obj], forcegravy[obj], diffy[obj];
-double accprevz[obj], forcecouloumbz[obj], forcegravz[obj], diffz[obj];
-double mass[obj], dist[obj], mag[obj];
-const double forceconstx = 0;
-const double forceconsty = -9.8067;
-const double gravconst = 6.67384;
+//For now, let's use obj-1 and dimensions-1. Hacky, but it works.
+int obj = 4;
+int dimensions = 1;
+double forceconst[2] = {0, -9.81};
 
+int i, j, width = 1200, height = 480;
 float dt = 0.01;
 
 
 
-
-
-int i, j, width = 1200, height = 480;
-
-
-
-int verlet() {
-	for(i = 1; i < obj; i++) {
-		if(posx[i] > width || posx[i] < 0) {
-			velx[i] = -velx[i];
+int integrate(double pos[][obj], double vel[][obj], double acc[][obj], double Ftot[][obj], double Fgrv[][obj], double Fele[][obj], double mass[obj]) {
+	double *accprev = malloc(dimensions * sizeof *accprev);
+	double *diff = malloc(dimensions * sizeof *diff);
+	for(i = 1; i < obj + 1; i++) {
+		//setting the bounds for a box. Do want a periodic boundary sometime down the road though...
+		if(pos[i][0] > width || pos[i][0] < 0) {
+			vel[i][0] = -vel[i][0];
 		}
-		if(posy[i] < 0 || posy[i] > height) {
-			vely[i] = -vely[i];
+		if(pos[i][1] < 0 || pos[i][1] > height) {
+			vel[i][1] = -vel[i][1];
 		}
-		posx[i] = posx[i] + (velx[i]*dt) + (accx[i])*((dt*dt)/2);
-		posy[i] = posy[i] + (vely[i]*dt) + (accy[i])*((dt*dt)/2);
+		for(j = 0; j < dimensions + 1; j++) {
+			pos[i][j] = pos[i][j] + (vel[i][j]*dt) + (acc[i][j])*((dt*dt)/2);
+		}
 	}
-	for(i = 1; i < obj; i++) {
-		forcex[i] = forceconstx;
-		forcey[i] = forceconsty;
+	for(i = 1; i < obj + 1; i++) {
+		for(j = 0; j < dimensions + 1; j++) {
+			Ftot[i][j] = forceconst[j];
+			//lets just naively assume this for now, k?
+		}
 	}
-	//printf("Obj 0's Force: %f || Obj 1's Force: %f || Obj 2's Force: %f\n", (forcex[1]+forcey[1]), (forcex[2]+forcey[2]), (forcex[3]+forcey[3]));
-	for(i = 1; i < obj; i++) {
-		accprevx[i] = accx[i];
-		accprevy[i] = accy[i];
-		accx[i] = (forcex[i])/(mass[i]);
-		accy[i] = (forcey[i])/(mass[i]);
-	}
-	for(i = 1; i < obj; i++) {
-		velx[i] = velx[i] + ((dt)/2)*(accx[i] + accprevx[i]);
-		vely[i] = vely[i] + ((dt)/2)*(accy[i] + accprevy[i]);
+	for(i = 1; i < obj + 1; i++) {
+		for(j = 0; j < dimensions + 1; j++) {
+			accprev[j] = acc[i][j];
+			acc[i][j] = (Ftot[i][j])/(mass[i]);
+			vel[i][j] = vel[i][j] + ((dt)/2)*(acc[i][j] + accprev[j]);
+			//Originally velocity was calculated in a loop below, but it's fine here and we save an extra dimension in accprev array.
+		}
 	}
 	return 0;
 }
 
 
 int main() {
-	//double (*pos)[dimensions] = malloc((dimensions) * sizeof *pos);
-	//double (*vel)[dimensions] = malloc((dimensions) * sizeof *vel);
-	posx[1] = 123;
-	posy[1] = 321;
-	posx[2] = 22;
-	posy[2] = 22;
-	posx[3] = 456;
-	posy[3] = 112;
-	velx[1] = 12;
-	vely[1] = 44;
-	velx[2] = 50;
-	vely[2] = 42;
-	velx[3] = 56;
-	vely[3] = 21;
+	double (*pos)[obj] = malloc(100*(obj*dimensions) * sizeof *pos);
+	double (*vel)[obj] = malloc(100*(obj*dimensions) * sizeof *vel);
+	double (*acc)[obj] = malloc(100*(obj*dimensions) * sizeof *acc);
+	double (*Ftot)[obj] = malloc(100*(obj*dimensions) * sizeof *Ftot);
+	double (*Fgrv)[obj] = malloc(100*(obj*dimensions) * sizeof *Fgrv);
+	double (*Fele)[obj] = malloc(100*(obj*dimensions) * sizeof *Fele);
+	double *mass = malloc(obj * sizeof *mass);
+	
 	mass[1] = 1;
+	pos[1][0] = 11;
+	pos[1][1] = 31;
+	vel[1][0] = 12;
+	vel[1][1] = 44;
+	
 	mass[2] = 1;
+	pos[2][0] = 22;
+	pos[2][1] = 222;
+	vel[2][0] = 22;
+	vel[2][1] = 5;
+	
 	mass[3] = 1;
-	//res[0] = 0.02;
-	//res[1] = 0.02;
+	pos[3][0] = 41;
+	pos[3][1] = 112;
+	vel[3][0] = 122;
+	vel[3][1] = 23;
+	
+	
 	
 	//SDL RELATED STUFF. A PAIN TO DEAL WITH.
 		SDL_Init(SDL_INIT_VIDEO);
@@ -142,27 +141,28 @@ int main() {
 			}
 		}
 		glClear(GL_COLOR_BUFFER_BIT);
-		verlet();
+		integrate(pos, vel, acc, Ftot, Fgrv, Fele, mass);
 		
 		
 		
 		//MAIN OBJECTS, RECTANCLES, ETC.
 		glBegin(GL_POINTS);
-			for(i = 1; i < obj; i++) {
-				glVertex3f( posx[i], posy[i], 1 );
+			for(i = 1; i < obj + 1; i++) {
+				glVertex3f( pos[i][0], pos[i][1], 1 );
 			}
 		glEnd();
 		glBegin(GL_LINES);
-			for(i = 1; i < obj; i++) {
-				glVertex3f( posx[i], posy[i], 1 );
-				glVertex3f( (posx[i] + velx[i]), (posy[i] + vely[i]), 1 );
+			for(i = 1; i < obj + 1; i++) {
+				glVertex3f( pos[i][0], pos[i][1], 1 );
+				glVertex3f( (pos[i][0] + vel[i][0]), (pos[i][1] + vel[i][1]), 1 );
 			}
 		glEnd();
 		
 		
 		//TEXT N STUFF GOES IN HERE.
-		txtRect.x = posx[1];
-		txtRect.y = posy[1];
+		txtRect.x = pos[1][0];
+		txtRect.y = pos[1][1];
+		printf("Obj1x = %f, Obj1y = %f\n", pos[1][0], pos[1][1]);
 		
 		imgTxt = TTF_RenderText_Blended( font , "HEAVY METAL" , fColor );
 		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, imgTxt);
@@ -179,17 +179,3 @@ int main() {
 	}
 return 0;
 }
-
-
-/*for(j = 1; j < obj; j++) {
-			if(i != j) {
-				diffx[i] = (posx[j] - posy[i]);
-				diffy[i] = (posy[j] - posy[i]);
-				dist[i] = sqrt(diffx[i]*diffx[i] + diffy[i]*diffy[i]);
-				mag[i] = (gravconst/(pow(1, 10)))*((mass[i]*mass[j])/(dist[i]*dist[i]));
-				forcegravx[i] = -(mag[i])*((diffx[i])/(dist[i]));
-				forcegravy[i] = -(mag[i])*((diffy[i])/(dist[i]));
-				forcex[i] = forceconstx + forcegravx[i];
-				forcey[i] = forceconsty + forcegravy[i];
-			}
-		}*/
