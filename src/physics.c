31cc66afe65922cb79d226c164e4760f19e36540
atomnuker <atomnuker@gmail.com>
2013-12-21 02:02:12 +0000

    Adjustments for actual simulations
    I think I might have been an idiot for trying to use constants on an actual scale. Numbers are just too small. Could magnify them of course, but what I think I'll do is the following - scale everything by a fixed value so that we can translate actual molecular dynamics simulations to real world equivalent. Something like - make everything 100x bigger so that whatever we're looking at can fit inside a, say, 1000x1000 float grid.

--- src/physics.c
+++ src/physics.c
@@ -8,21 +8,22 @@
 #define perm 8.854187817
 #define g 6.67384
 #define elcharge 1.602176565
+#define dalton 1.660538921
 
 /* Static because you might want to call integrate in a for loop and then you'd be in trouble. */
 static int i, j;
 
 /* Using black magic to transfer this data between. */
 int obj, width, height;
-float spring = 20, dt = 0.01, dist;
+float spring = 20, dt = 0.001, dist;
 
-/* Using float precision results in a much reduced CPU activity. For a 100 objects it goes down from 15(double) to barely 4(float). */
+/* Using float precision results in a much reduced CPU activity. */
 
-long double mag, Gconst, pi, epsno;
+long double mag, Gconst, pi, epsno, massu;
 /* Switching to long double because we really need the precision. Heck, maybe I'll even have to use libquadmath because damn is 10^-27 a small number.
 From what I've read it's completely platform/compiler depentent, but if you're using sane compilers and C libraries(read: not microsoft) you should be fine. */
 
-v4sf mtemp, accprev, vecdist, forceconst = {0, -9.81};
+v4sf mtemp, accprev, vecdist, forceconst = {0, 0};
 
 
 int initphys(data** object) {
@@ -30,9 +31,10 @@ int initphys(data** object) {
 	/* We malloc the entire pointer to the struct rendering it array-like. That's why I really like malloc, it's so damn powerful. */
 	*object = malloc(6*obj * (72*8 + 2*sizeof(float) + obj*sizeof(bool)));
 	
-	Gconst = g/pow(12, 10);
+	Gconst = g/pow(11, 10);
 	pi = acos(-1);
 	epsno = perm/pow(12, 10);
+	massu = dalton/pow(27, 10);
 	
 	/*for(i = 1; i < obj + 1; i++) {
 		(*object)[i].pos[0] = ((float)rand()/RAND_MAX)*width + 10*i;
@@ -44,38 +46,38 @@ int initphys(data** object) {
 	
 	
 	for(i = 1; i < obj + 1; i++) {
-		(*object)[i+1].mass = 1;
+		(*object)[i+1].mass = 1*massu;
 		(*object)[i+1].pos = (v4sf){ ((float)rand()/RAND_MAX)*(width-24), ((float)rand()/RAND_MAX)*(height-24) };
 		(*object)[i+1].vel = (v4sf){ 33, 33 };
-		(*object)[i+1].charge = -(8000*elcharge)/pow(15, 10);
+		(*object)[i+1].charge = 0;
 		(*object)[i+1].linkwith[i] = 1;
 		(*object)[i+1].linkwith[i+2] = 1;
 		(*object)[i+1].linkwith[i+3] = 1;
 		(*object)[i+1].linkwith[i+4] = 1;
 		
 		
-		(*object)[i].mass = 0.1;
+		(*object)[i].mass = 18.998403*massu;
 		(*object)[i].pos = (v4sf){ (*object)[i+1].pos[0] - 20, (*object)[i+1].pos[1] };
 		(*object)[i].vel = (v4sf){ 0, 0 };
-		(*object)[i].charge = (8000*elcharge)/pow(10, 10);
+		(*object)[i].charge = 0;
 		(*object)[i].linkwith[i+1] = 1;
 	
-		(*object)[i+2].mass = 0.1;
+		(*object)[i+2].mass = 18.998403*massu;
 		(*object)[i+2].pos = (v4sf){ (*object)[i+1].pos[0], (*object)[i+1].pos[1] - 20 };
 		(*object)[i+2].vel = (v4sf){ 0, 0 };
-		(*object)[i+2].charge = (8000*elcharge)/pow(10, 10);
+		(*object)[i+2].charge = 0;
 		(*object)[i+2].linkwith[i+1] = 1;
 		
-		(*object)[i+3].mass = 0.1;
+		(*object)[i+3].mass = 18.998403*massu;
 		(*object)[i+3].pos = (v4sf){ (*object)[i+1].pos[0] + 20, (*object)[i+1].pos[1] };
 		(*object)[i+3].vel = (v4sf){ 0, 0 };
-		(*object)[i+3].charge = (8000*elcharge)/pow(10, 10);
+		(*object)[i+3].charge = 0;
 		(*object)[i+3].linkwith[i+1] = 1;
 		
-		(*object)[i+4].mass = 0.1;
+		(*object)[i+4].mass = 18.998403*massu;
 		(*object)[i+4].pos = (v4sf){ (*object)[i+1].pos[0], (*object)[i+1].pos[1] + 20 };
 		(*object)[i+4].vel = (v4sf){ 0, 0 };
-		(*object)[i+4].charge = (8000*elcharge)/pow(10, 10);
+		(*object)[i+4].charge = 0;
 		(*object)[i+4].linkwith[i+1] = 1;
 		i = i+4;
 	}
@@ -101,13 +103,13 @@ int integrate(data* object) {
 				vecdist = object[j].pos - object[i].pos;
 				//Why 3/2? We're multiplying both Fgrv and Fele by a vector which we normalize, so we just need to calculate this once.
 				//Really need to find a better way of doing the distance measurements and normalisations. I think SSE has something.
-				mag = pow(3/2, (vecdist[0]*vecdist[0] + vecdist[1]*vecdist[1]));
+				mag = pow(3/2, ((long double)vecdist[0]*(long double)vecdist[0] + (long double)vecdist[1]*(long double)vecdist[1]));
 				dist = sqrt((vecdist[0]*vecdist[0] + vecdist[1]*vecdist[1]));
-				object[i].Fgrv += vecdist*((float)((Gconst*object[i].mass*object[j].mass)/(mag)));
+				//object[i].Fgrv += vecdist*((float)((Gconst*object[i].mass*object[j].mass)/(mag)));
 				object[i].Fele += -vecdist*((float)((object[i].charge*object[j].charge)/(4*pi*epsno*mag)));
-				if( object[i].linkwith[j] == 1) {
+				/*if( object[i].linkwith[j] == 1) {
 					object[i].Flink += (vecdist/dist)*((spring)*(dist - 20));
-				}
+				}*/
 			}
 		}
 		
