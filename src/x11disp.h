bf2f32d070c5c1c6b2fae6f2756419e5ea666052
atomnuker <atomnuker@gmail.com>
2014-02-12 21:35:54 +0000

    Removed redundant X11 disp. funct.
    Since we're now trusting SDL2 to create a window and bind OpenGL (ES in case system does not support full OGL), we don't need to dig into X11 directly. I can already feel the life coming back to me.

--- src/x11disp.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTIONS_L_INCLUDED
-#define FUNCTIONS_L_INCLUDED
-
-void x11disp(Display *x_dpy, EGLDisplay egl_dpy, const char *name, int x, int y, int width, int height, Window *winRet, EGLContext *ctxRet, EGLSurface *surfRet);
-
-#endif
