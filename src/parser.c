d4f749b51a2cd9f54ad80570edfd1139dfad70bf
atomnuker <atomnuker@gmail.com>
2014-02-22 01:22:43 +0000

    parser: random distribution changed to [-1,1]

--- src/parser.c
+++ src/parser.c
@@ -165,7 +165,7 @@ int parser(data** object, char filename[200])
 		}
 	} else if ( random == 1 ) {
 		for(i = 1; i < obj + 1; i++) {
-			(*object)[i].pos = (v4sf){((float)rand()/(float)RAND_MAX), ((float)rand()/(float)RAND_MAX), ((float)rand()/(float)RAND_MAX)};
+			(*object)[i].pos = (v4sf){((float)rand()/(float)RAND_MAX) - 0.5, ((float)rand()/(float)RAND_MAX) - 0.5, ((float)rand()/(float)RAND_MAX) - 0.5};
 			(*object)[i].vel = (v4sf){(((float)rand()/(float)RAND_MAX) - 0.5)*velmax, \
 			(((float)rand()/(float)RAND_MAX) - 0.5)*velmax, (((float)rand()/(float)RAND_MAX) - 0.5)*velmax};
 			(*object)[i].mass = (((float)rand()/(float)RAND_MAX))*massrand;
