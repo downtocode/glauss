9be37b254f619604ebcbc2021673c3ed44164e12
atomnuker <atomnuker@gmail.com>
2014-03-19 15:03:26 +0000

    pprintf: added prioritized message system
    Dial in the verbosity and off it spams stdout.

---
+++ src/msg_phys.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "msg_phys.h"
+#include "options.h"
+
+void pprint(unsigned int priority, const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	if(priority <= option->verbosity) {
+		vprintf (format, args);
+	}
+	va_end(args);
+}
---
+++ src/msg_phys.h
@@ -0,0 +1,6 @@
+#ifndef PHYSENGINE_MSG
+#define PHYSENGINE_MSG
+
+void pprint(unsigned int priority, const char *format, ...);
+
+#endif
