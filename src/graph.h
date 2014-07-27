1e561178ced6b30789eca7bfa549d662fbac5076
atomnuker <atomnuker@gmail.com>
2014-07-27 20:01:14 +0100

    graph: rearragned header

--- src/graph.h
+++ src/graph.h
@@ -18,6 +18,16 @@
 #ifndef PHYSENGINE_GRAPH
 #define PHYSENGINE_GRAPH
 
+#include "physics.h"
+
+/* Camera info sent from main */
+struct graph_cam_view {
+	float view_rotx, view_roty, view_rotz;
+	float tr_x, tr_y, tr_z;
+	float scalefactor;
+};
+
+/* Identifiers for colors */
 enum {
 	GL_WHITE,
 	GL_RED,
@@ -26,26 +36,18 @@ enum {
 	GL_YELLOW,
 };
 
+/* Color array */
 extern const float white[];
 extern const float red[];
 extern const float green[];
 extern const float blue[];
 extern const float yellow[];
 
-struct graph_cam_view {
-	float view_rotx, view_roty, view_rotz;
-	float tr_x, tr_y, tr_z;
-	float scalefactor;
-};
-
 void graph_init();
 void graph_view(struct graph_cam_view *camera);
 float graph_resize_wind();
 unsigned int graph_compile_shader(const char *src_vert_shader,
 								  const char *src_frag_shader);
-
-#include "physics.h"
-
 void graph_draw_scene(data **object, float fps, unsigned int chosen);
 
 #endif
